9ad709e2b4656ef151cb39d758f2cde801b43eba
Kevin Pasquarella <pasquarella4292@gmail.com>
2020-05-13 18:48:01 -0400

    initial commit

---
+++ src/client/MQTTSub.java
@@ -0,0 +1,10 @@
+import org.eclipse.paho.mqttv3.MqttAsyncClient;
+
+public class MQTTSub extends MqttAsyncClient {
+
+    private String serverURI;
+    
+    public MQTTSub(String serverURI) {
+
+    }
+}
