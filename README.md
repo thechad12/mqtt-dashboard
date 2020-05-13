382d1b715da6e0531144e39474bf303c9797dbf3
Kevin Pasquarella <pasquarella4292@gmail.com>
2020-05-13 18:54:00 -0400

    add readme. start building the mqtt subscriber. next will be testing, then building the servlet and views

---
+++ README.md
@@ -0,0 +1,4 @@
+**MQTT Dashboard**
+
+_A lightweight dashboard for mqtt data_
+ - Intended usage: Pulling MQTT Data from home devices or small office/production areas.
--- src/client/MQTTSub.java
+++ src/client/MQTTSub.java
@@ -1,10 +1,18 @@
 import org.eclipse.paho.mqttv3.MqttAsyncClient;
+import org.eclipse.paho.mqttv3.MqttConnectOptions;
+
+import java.util.UUID;
 
 public class MQTTSub extends MqttAsyncClient {
 
     private String serverURI;
-    
-    public MQTTSub(String serverURI) {
+    private String clientID = UUID.randomUUID().toString();
 
+    public MQTTSub(String serverURI) {
+        this.serverURI = serverURI;
+        super(this.serverURI,this.clientID);
+        super.connect();
     }
+
+    
 }
